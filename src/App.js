28417adc551ff1092648162715507b42e0e7c45a
Acteon-25 <jeancoaquira@hotmail.com>
2024-05-04 17:01:47 -0500

    FinalClase2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import './App.css';
 
 function App() {
   return (
+
     <div className='from_container'>
       <div className='form_header'>
         <h1 className='form_title'>Mi primer formulario</h1>
+      </div>
+      <div className='form_body'>
+        <div className='form'>
+          <label className='form_label'>
+            Nombres y apellidos:
+          </label>
+          <div className='form_input_ico'>
+            <i className="fa fa-user" aria-hidden="true">
+              <input className='from_input'>
+
+              </input>
+            </i>
+          </div>
+        </div>
       </div>
     </div>
   );
