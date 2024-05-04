6561ca0d2ca463357df0e12fee8093c3c4f2682a
Acteon-25 <jeancoaquira@hotmail.com>
2024-05-04 16:29:58 -0500

    2da clase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ZZZzzzz
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='from_container'>
+      <div className='form_header'>
+        <h1 className='form_title'>Mi primer formulario</h1>
+      </div>
     </div>
   );
 }
